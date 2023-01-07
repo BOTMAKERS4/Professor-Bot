7c703507d2080407abb6057ec20b9047f13ea78f
Harsh Jaiswal <harshjais7864@gmail.com>
2023-01-07 20:03:29 +0530

    FreeCommit3

--- userbot/plugins/sql_helper/openaiconfig_sql.py
+++ userbot/plugins/sql_helper/openaiconfig_sql.py
@@ -3,19 +3,19 @@ from userbot.plugins.sql_helper import BASE, SESSION
 
 class OpenaiConfig(BASE):
     __tablename__ = "openai_config"
-    model_id = Column(Integer, primary_key=True, default=1)
-    model = Column(String(14), primary_key=True, default="text-davinci-003")
-    temperature = Column(String(14), default="0.7")
-    max_tokens = Column(String(14), default="256")
-    top_p = Column(String(14), default="1")
-    frequency_penalty = Column(String(14), default="0")
-    presence_penalty = Column(String(14), default="0")
+    model_id = Column(Integer, primary_key=True)
+    model = Column(String(14))
+    temperature = Column(String(14))
+    max_tokens = Column(String(14))
+    top_p = Column(String(14))
+    frequency_penalty = Column(String(14))
+    presence_penalty = Column(String(14))
 
     def __init__(self,
         model_id,
         model="text-davinci-003"
         temperature="0.7"
-        max_tokens="256"
+        max_tokens="2048"
         top_p="1"
         frequency_penalty="0"
         presence_penalty="0"
@@ -28,9 +28,53 @@ class OpenaiConfig(BASE):
         self.frequency_penalty = frequency_penalty
         self.presence_penalty = presence_penalty
 
+    def __repr__(self):
+        return "<OpenaiConfig(model_id=%d, model='%s', temperature='%s', max_tokens='%s', top_p='%s', frequency_penalty='%s', presence_penalty='%s')>"
+            % (int(self.model_id), self.model, self.temperature, self.max_tokens, self.top_p, self.frequency_penalty, self.presence_penalty)
 
 OpenaiConfig.__table__.create(checkfirst=True)
 
+
+def setOpenaiConfig(model_name, temperature, max_tokens, top_p, frequency_penalty, presence_penalty):
+    data = SESSION.query(OpenaiConfig).filter(int(OpenaiConfig.model_id) == 1).first()
+    if data is None:
+        session.add(OpenaiConfig(
+            model_id=1
+            model="text-davinci-003"
+            temperature="0.7"
+            max_tokens="2048"
+            top_p="1"
+            frequency_penalty="0"
+            presence_penalty="0"
+        ))
+        session.commit()
+    data.update({
+        OpenaiConfig.model_name=model_name,
+        OpenaiConfig.temperature = temperature,
+        OpenaiConfig.max_tokens = max_tokens,
+        OpenaiConfig.top_p = top_p,
+        OpenaiConfig.frequency_penalty = frequency_penalty,
+        OpenaiConfig.presence_penalty = presence_penalty,
+    })
+    SESSION.close()
+    return True
+
+def getOpenaiConfig():
+    data = SESSION.query(OpenaiConfig).filter(int(OpenaiConfig.model_id) == 1).first()
+    if data is None:
+        session.add(OpenaiConfig(
+            model_id=1
+            model="text-davinci-003"
+            temperature="0.7"
+            max_tokens="2048"
+            top_p="1"
+            frequency_penalty="0"
+            presence_penalty="0"
+        ))
+        session.commit()
+    data2 = SESSION.query(OpenaiConfig).all()[0]
+    res_list = [data2.model]
+    return res_list
 
 def is_approved(chat_id):
     try:
