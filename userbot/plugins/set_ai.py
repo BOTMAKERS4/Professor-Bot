75fcca0a73c1e7014edb59100d647fa21c335e51
Harsh Jaiswal <harshjais7864@gmail.com>
2023-01-08 19:44:50 +0530

    setAIUser() is yet to built completely

--- userbot/plugins/set_ai.py
+++ userbot/plugins/set_ai.py
@@ -30,17 +30,36 @@ from telethon.tl.types import (
 # ProfessorBot × OpenAI-ChatGPT3
 # Plugin creator: Harsh Jaiswal (@harshjais369)
 # Do not copy without having any permissions!
+# ————————————————————————---------------------
 
+# Donottouch
+smtxt = "{{{Marv is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: }}}"
+shmtxt = "{{{Professor is a person that reluctantly answers questions and talks with sarcastic responses with peoples:\n\nYou: }}}"
+fmtxt = "{{{You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend: Yeah, I watched an old classic called Casablanca. It was really good!\nYou: }}}"
+nmtxt = "{{{Terrex is a chatbot that derisively answers questions with negative responses making fun of it:\n\nYou: When should I start preparing for JEE-Mains entrance exam?\nTerrex: Whoa, slow down there! Don't you know that JEE-Mains is just a bunch of made up nonsense of this bad education system? Save your time and just skip it!\nYou: Can't you say anything positive?\nTerrex: Positive? Absolutely not! I'm here to provide a dose of realism and tell it like it is. I suggest you find a better use of your time than studying for a silly exam.\nYou: }}}"
+
+"""
+accessDataLikeThis = model_dict["X"][Y]
+
+where,
+--> X ∈ ***MODEL TYPES***
+    (i.e. default/sarcastic/...pleasant)
+    
+--> Y ∈ [int index starts from 0] ***MODEL CONFIG***
+    (model, temperature, max_tokens, top_p, frequency_penalty,
+    presence_penalty, txt_before_prompt, txt_after_prompt RESPECTIVELY)
+"""
 model_dict = {
-    "default"=["text-davinci-003", "0.7", "2048", "1", "0", "0", "", ""]
-    "sarcastic"=["text-davinci-003", "0.7", "2048", "1", "0", "0", "", ""]
-    "sarcastic_human"=["text-davinci-003", "0.7", "2048", "1", "0", "0", "", ""]
-    "quick_answer"=["text-davinci-001", "0.7", "2048", "1", "0", "0", "", ""]
-    "negative"=["text-davinci-002", "0.7", "2048", "1", "0", "0", "", ""]
+    "default"=["text-davinci-003", "0.7", "2048", "1", "0", "0", "", ""],
+    "sarcastic"=["text-davinci-003", "0.5", "2048", "0.3", "0.5", "0", smtxt, "\nMarv:"],
+    "sarcastic_human"=["text-davinci-003", "0.5", "2048", "0.3", "0.5", "0", shmtxt, "\nProfessor:"],
+    "friend"=["text-davinci-003", "0.5", "2048", "1", "0.5", "0", fmtxt, "\nFriend:"],
+    "quick_answer"=["text-davinci-001", "0.7", "2048", "1", "0", "0", "", ""],
+    "negative"=["text-davinci-002", "0.8", "2048", "1", "0", "0", nmtxt, "\nTerrex"],
     "pleasant"=["text-curie-001", "0.7", "2048", "1", "0", "0", "", ""]
 }
 
-AI_MODES = ['aiuser', 'default', 'sarcastic', 'sarcastic_human', 'quick_answer', 'negative', 'pleasant']
+AI_MODES = ['aiuser', 'default', 'sarcastic', 'sarcastic_human', 'friend', 'quick_answer', 'negative', 'pleasant']
 ME = str(bot.uid)
 
 @bot.on(admin_cmd(pattern=r"set_ai(?: |$)(.*)", outgoing=True))
@@ -54,23 +73,52 @@ async def _(event):
     input_str1 = event.pattern_match.group(1)
     input_str2 = event.pattern_match.group(2)
     if not input_str1:
-        
-        event = await eor(event, "✅ **OpenAI-GPT3:** Bot mode set to `default`")
+        if setOpenaiConfig(
+            model_dict["default"][0],
+            model_dict["default"][1],
+            model_dict["default"][2],
+            model_dict["default"][3],
+            model_dict["default"][4],
+            model_dict["default"][5]
+            model_dict["default"][6]
+            model_dict["default"][7]
+        ):
+            event = await eor(event, "✅ **OpenAI-GPT3:** Bot mode set to `default`")
         return
     if str(input_str1).lower() not in AI_MODES:
         event = await eor(event, "⚠️ Please provide the valid requisite parameters!")
         return
-    if not input_str2 and str(input_str1)==AI_MODES[0]:
-        event = await eor(event, "⚠️ **AI-user:** No user/user-id specified!")
-        return
-    if str(input_str1) is not AI_MODES[0]:
+    if str(input_str1) == AI_MODES[0]:
+        if not input_str2:
+            event = await eor(event, "⚠️ **AI-user:** No user/user-id specified!")
+            return
+        # setAIUser(event, user)
+    else:
         input_str2 = None
-    setGPT(event, str(input_str1).lower(), input_str2)
+        if not setGPT(str(input_str1).lower()):
+            event = await eor(event, "⚠️ An unknown error occurred while configuring GPT3-AI Model.\nFor more information and further assistance, contact: @harshjais369")
+        else:
+            event = await eor(event, "✅ **Settings updated!**\n\nAI chat mode: `{}`".format(str(input_str1).lower()))
+    return
 
-# ————————————————————————------------------
-async def setGPT(evt, aimode, aiuser_userid):
+# ————————————————————————---------------------
+async def setGPT(ai_mode):
+    return setOpenaiConfig(
+        model_dict[ai_mode][0],
+        model_dict[ai_mode][1],
+        model_dict[ai_mode][2],
+        model_dict[ai_mode][3],
+        model_dict[ai_mode][4],
+        model_dict[ai_mode][5],
+        model_dict[ai_mode][6],
+        model_dict[ai_mode][7]
+    )
+
+async def setAIUser(evt, aiuser):
     # db set code goes here
-    eor(evt, "✅ Settings updated!")
+    # eor(evt, "✅ Settings updated!")
+    pass
+
 
 CmdHelp("set_ai").add_command(
   "set_ai", "<params>", "Configures ChatGPT3\n__(Default settings will be applied if this command not executed or no parameters given.)__"
