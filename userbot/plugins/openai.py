432d6ccd416d38f8929f773718ef8faabed5520f
Harsh Jaiswal <harshjais7864@gmail.com>
2023-01-08 16:17:42 +0530

    Create openai.py

---
+++ userbot/plugins/openai.py
@@ -0,0 +1,45 @@
+from mafiabot.utils import admin_cmd, sudo_cmd, edit_or_reply as eor
+from mafiabot import CmdHelp
+from asyncio import sleep
+from math import ceil
+from re import compile
+import asyncio
+from telethon.events import InlineQuery, callbackquery
+from telethon.sync import custom
+from telethon import functions
+from userbot import bot
+from userbot.cmdhelp import *
+from mafiabot.utils import *
+from userbot.Config import Config
+from . import *
+from telethon.errors.rpcerrorlist import UserAdminInvalidError, UserIdInvalidError
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.errors import (
+    BadRequestError,
+    ImageProcessFailedError,
+    PhotoCropSizeSmallError,
+)
+from telethon.tl.types import (
+    ChatAdminRights,
+    ChatBannedRights,
+    MessageEntityMentionName,
+    MessageMediaPhoto,
+)
+
+# ProfessorBot × OpenAI-ChatGPT3
+# Plugin creator: Harsh Jaiswal (@harshjais369)
+# Do not copy without having any permissions!
+
+AI_MODES = ['aiuser', 'default', 'sarcastic', 'sarcastic_human', 'quick_answer']
+ME = str(bot.uid)
+
+@bot.on(admin_cmd(pattern="q(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="q(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await eor(event, "**HELLO WORLD!**")
+
+CmdHelp("q").add_command(
+  "q", "<your question>", "Talk to ChatGPT3 AI."
+).add
