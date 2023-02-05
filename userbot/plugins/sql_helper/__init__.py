31dd8d897d8bac0425614e55d71b5789114fd42b
Harsh Jaiswal <harshjais369@gmail.com>
2023-02-05 14:48:39 +0530

    Update __init__.py

--- userbot/plugins/sql_helper/__init__.py
+++ userbot/plugins/sql_helper/__init__.py
@@ -1,13 +1,12 @@
 import os
-
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-# the secret configuration specific things
 from var import Var
 from userbot.Config import Config
 
+load_dotenv(verbose=True)
 DB_URI = os.environ.get("DATABASE_URL", None)
 
 def start() -> scoped_session:
@@ -25,9 +24,7 @@ def start() -> scoped_session:
 try:
     BASE = declarative_base()
     SESSION = start()
-except AttributeError as e:
+except Exception as e:
     # this is a dirty way for the work-around required for #23
-    print(
-        "DB_URI is not configured. Features depending on the database might have issues."
-    )
+    print("DB_URI is not configured. Features depending on the database might have issues.")
     print(str(e))
